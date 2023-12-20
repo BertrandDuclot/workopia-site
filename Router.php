a536f4b34706c2894403f2b116b44fa77970f4c0
Bertrand D <b.duclot@gmail.com>
2023-12-20 11:01:26 +0100

    update

---
+++ Router.php
@@ -0,0 +1,54 @@
+<?php
+
+class Router {
+    protected $routes = [];
+
+    /**
+     * Add a GET route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function get($uri, $controller) {
+        $this->routes = [
+            'method' => 'GET',
+            'uri' => $uri,
+            'controller' => $controller
+        ];
+
+    }
+
+    /**
+     * Add a POST route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function post($uri, $controller) {
+        
+    }
+
+    /**
+     * Add a PUT route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function put($uri, $controller) {
+        
+    }
+
+    /**
+     * Add a DELETE route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function delete($uri, $controller) {
+        
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,18 +1,7 @@
 <?php
 require '../helpers.php';
 
-$routes = [
-    '/' => 'controllers/home.php',
-    '/listings' => 'controllers/listings/index.php',
-    '/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php'
-];
-
 $uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
 
-if(array_key_exists($uri, $routes)){
-    require(basePath($routes[$uri]));
-} else {
-    //echo basePath($routes['$uri']);
-    require basePath($routes['404']);
-}
+require basePath('router.php');
